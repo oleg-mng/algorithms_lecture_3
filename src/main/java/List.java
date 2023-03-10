bacf87c2321f1a3565ba1ada06146252e44e84f8
oleg <legik7127@gmail.com>
2023-03-10 18:18:53 +0600

    added method delete

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,84 +1,96 @@
 public class List {
     Node head;
-    Node tail;
+//    Node tail;
 
-    public void add(int value) {
-        Node node = new Node();
-        node.value = value;
-        if (head == null) {
-            head = node;
-            tail = node;
-        } else {
-            tail.next = node;
-            node.previous = tail;
-            tail = node;
-        }
-    }
+//    public void add(int value) {
+//        Node node = new Node();
+//        node.value = value;
+//        if (head == null) {
+//            head = node;
+//            tail = node;
+//        } else {
+//            tail.next = node;
+//            node.previous = tail;
+//            tail = node;
+//        }
+//    }
+//
+//    public void add(int value, Node node) {
+//        Node next = node.next;
+//        Node newNode = new Node();
+//        newNode.value = value;
+//        node.next = newNode;
+//        newNode.previous = node;
+//        if (next == null) {
+//            tail = newNode;
+//        }
+//        else {
+//            next.previous = newNode;
+//            newNode.next = next;
+//        }
+//    }
+//    public void delete(Node node){
+//        Node previous = node.previous;
+//        Node next = node.next;
+//        if (previous == null){
+//            next.previous = null;
+//            head = next;
+//        }
+//        else {
+//            if (next == null){
+//                previous.next = null;
+//                tail = previous;
+//            }
+//            else {
+//                previous.next = next;
+//                next.previous = previous;
+//            }
+//        }
+//    }
+//
+//    public Node find(int value) {
+//        Node currentNode = head;
+//        while (currentNode != null) {
+//            if (currentNode.value == value) {
+//                return currentNode;
+//            }
+//            currentNode = currentNode.next;
+//        }
+//        return null;
+//    }
+//    public void revert(){
+//        Node currentNode = head;
+//        while (currentNode != null){
+//            Node next = currentNode.next;
+//            Node previous = currentNode.previous;
+//            currentNode.next = previous;
+//            currentNode.previous = next;
+//            if (previous == null){
+//                tail = currentNode;
+//            }
+//            if (next == null){
+//                head = currentNode;
+//            }
+//            currentNode = next;
+//        }
+//    }
 
-    public void add(int value, Node node) {
-        Node next = node.next;
-        Node newNode = new Node();
-        newNode.value = value;
-        node.next = newNode;
-        newNode.previous = node;
-        if (next == null) {
-            tail = newNode;
-        }
-        else {
-            next.previous = newNode;
-            newNode.next = next;
-        }
-    }
-    public void delete(Node node){
-        Node previous = node.previous;
-        Node next = node.next;
-        if (previous == null){
-            next.previous = null;
-            head = next;
-        }
-        else {
-            if (next == null){
-                previous.next = null;
-                tail = previous;
-            }
-            else {
-                previous.next = next;
-                next.previous = previous;
-            }
-        }
-    }
-
-    public Node find(int value) {
-        Node currentNode = head;
-        while (currentNode != null) {
-            if (currentNode.value == value) {
-                return currentNode;
-            }
-            currentNode = currentNode.next;
-        }
-        return null;
-    }
     public void revert(){
-        Node currentNode = head;
-        while (currentNode != null){
-            Node next = currentNode.next;
-            Node previous = currentNode.previous;
-            currentNode.next = previous;
-            currentNode.previous = next;
-            if (previous == null){
-                tail = currentNode;
-            }
-            if (next == null){
-                head = currentNode;
-            }
-            currentNode = next;
-        }
+        if (head!= null )
     }
-
+    private void revert (Node currentNode, Node previousNode){
+        if (currentNode.next == null){
+            head = currentNode;
+        }
+        else {
+            revert(currentNode.next, currentNode);
+        }
+        currentNode.next = previousNode;
+    }
     public class Node {
         int value;
         Node next;
-
-        Node previous;
+//
+//        Node previous;
     }
 }
