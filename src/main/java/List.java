ce39358102fa087e8040ef97999e1e3f0dd84aae
oleg <legik7127@gmail.com>
2023-03-10 15:46:53 +0600

    Added class Node

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,7 +1,34 @@
 public class List {
     Node head;
+    Node tail;
+    public void add(int value) {
+        Node node = new Node();
+        node.value = value;
+        if (head == null){
+            head = node;
+            tail = node;
+        }
+        else {
+            tail.next = node;
+            node.previous = tail;
+            tail = node;
+        }
+
+    }
+    public Node find(int value) {
+        Node currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.value == value) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
     public class Node {
         int value;
         Node next;
+
+        Node previous;
     }
 }
